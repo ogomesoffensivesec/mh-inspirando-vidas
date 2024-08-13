b26686fbcdd6cd422fe86ed3f305eb60c4c59fc7
ogomesoffensivesec <gomes.rootkit@gmail.com>
2024-08-12 22:44:42 -0300

    feat:gallery added and new phone contact

---
+++ assets/js/modal.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+
+function openModal(n) {
+  document.getElementById("myModal").style.display = "block";
+  showSlides(slideIndex = n);
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  if (n >= slides.length) slideIndex = 0;
+  if (n < 0) slideIndex = slides.length - 1;
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+}
